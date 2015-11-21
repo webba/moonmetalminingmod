764add43004455815473477a9d0297d4e5f01c99
Webba <webba93@gmail.com>
2015-11-21 21:53:14 +0000

    Changed removeMoonMetalVeinCap to changeVeinCap allowing choice of cap on moon metal veins. Separated chances of each moon metal to allow choice of rarity of each and added seryll lump as there is no ore.

--- src/com/webbrar/wurm/mods/moonmetalmining/MoonMetalMiningMod.java
+++ src/com/webbrar/wurm/mods/moonmetalmining/MoonMetalMiningMod.java
@@ -1,4 +1,4 @@
-package com.webbrar.wurm.mods;
+package com.webbrar.wurm.mods.moonmetalmining;
 
 import com.wurmonline.server.Server;
 import com.wurmonline.server.items.Item;
@@ -27,10 +27,12 @@ import org.gotti.wurmunlimited.modloader.interfaces.WurmMod;
  */
 public class MoonMetalMiningMod implements WurmMod, Configurable, PreInitable {
     private boolean useMoonMetalMiningMod = false;
-    private boolean removeVeinCap = false;
+    private boolean changeVeinCap = false;
+    private int newVeinCap = 10000;
     private boolean randomMoonMetalDrops = false;
-    private static boolean staticRandomMoonMetalDrops = false;
-    private static int staticRandomDropChance = 1000;
+    private static int staticRandomGlimmersteelChance = 3000;
+    private static int staticRandomAdamantiteChance = 3000;
+    private static int staticRandomSeryllChance = 3000;
     private String actionMethodDesc = "(Lcom/wurmonline/server/behaviours/Action;Lcom/wurmonline/server/creatures/Creature;Lcom/wurmonline/server/items/Item;IIZIISF)Z";
     private String createGemMethodDesc = "(IIIILcom/wurmonline/server/creatures/Creature;DZLcom/wurmonline/server/behaviours/Action;)Lcom/wurmonline/server/items/Item;";
     private Logger logger = Logger.getLogger(this.getClass().getName());
@@ -38,16 +40,21 @@ public class MoonMetalMiningMod implements WurmMod, Configurable, PreInitable {
     @Override
     public void configure(Properties properties) {
         useMoonMetalMiningMod = Boolean.valueOf(properties.getProperty("useMoonMetalMiningMod", Boolean.toString(useMoonMetalMiningMod)));
-        removeVeinCap = Boolean.valueOf(properties.getProperty("removeVeinCap", Boolean.toString(removeVeinCap)));
+        changeVeinCap = Boolean.valueOf(properties.getProperty("changeVeinCap", Boolean.toString(changeVeinCap)));
+        newVeinCap = Integer.valueOf(properties.getProperty("newVeinCap", Integer.toString(newVeinCap)));
+        if (newVeinCap < 0 ){
+            newVeinCap = 0;
+        }
         randomMoonMetalDrops = Boolean.valueOf(properties.getProperty("randomMoonMetalDrops", Boolean.toString(randomMoonMetalDrops)));
-        MoonMetalMiningMod.staticRandomMoonMetalDrops = randomMoonMetalDrops;
-        MoonMetalMiningMod.staticRandomDropChance = Integer.valueOf(properties.getProperty("randomDropChance", Integer.toString(MoonMetalMiningMod.staticRandomDropChance)));
+        MoonMetalMiningMod.staticRandomGlimmersteelChance = Integer.valueOf(properties.getProperty("randomGlimmersteelDropChance", Integer.toString(MoonMetalMiningMod.staticRandomGlimmersteelChance)));
+        MoonMetalMiningMod.staticRandomAdamantiteChance = Integer.valueOf(properties.getProperty("randomAdamantiteDropChance", Integer.toString(MoonMetalMiningMod.staticRandomGlimmersteelChance)));
+        MoonMetalMiningMod.staticRandomSeryllChance = Integer.valueOf(properties.getProperty("randomSeryllDropChance", Integer.toString(MoonMetalMiningMod.staticRandomSeryllChance)));
     }
     
     @Override
     public void preInit() {
         if(useMoonMetalMiningMod){
-            if(removeVeinCap){
+            if(changeVeinCap){
                 removeMoonMetalVeinCap();
             }
             if(randomMoonMetalDrops){
@@ -74,18 +81,31 @@ public class MoonMetalMiningMod implements WurmMod, Configurable, PreInitable {
                         boolean surfaced = (boolean)args[6];
                         Action act = (Action)args[7];
                         final byte rarity = (byte)((act != null) ? act.getRarity() : 0);
-                        if(MoonMetalMiningMod.staticRandomMoonMetalDrops){
-                            if (Server.rand.nextInt(MoonMetalMiningMod.staticRandomDropChance) == 0) {
-                                final int templateID = ((Server.rand.nextInt(2)==0) ? 693: 697);
-                                if (tilex < 0 && tiley < 0) {
-                                    final Item metal = ItemFactory.createItem(templateID, (float)power, (String)null);
-                                    metal.setLastOwnerId(performer.getWurmId());
-                                    return metal;
-                                }
-                                final Item metal = ItemFactory.createItem(templateID, (float)power, (float)(createtilex * 4 + Server.rand.nextInt(4)), (float)(createtiley * 4 + Server.rand.nextInt(4)), Server.rand.nextFloat() * 360.0f, surfaced, rarity, -10L, (String)null);
-                                metal.setLastOwnerId(performer.getWurmId());
-                                performer.getCommunicator().sendNormalServerMessage("You find a chunk of a mysterious metal.");
+                        int tempid = 0;
+                        if(MoonMetalMiningMod.staticRandomAdamantiteChance > 0){
+                            if (Server.rand.nextInt(MoonMetalMiningMod.staticRandomAdamantiteChance) == 0) {
+                                tempid = 693;
                             }
+                        }
+                        if(MoonMetalMiningMod.staticRandomGlimmersteelChance > 0){
+                            if (tempid == 0 && Server.rand.nextInt(MoonMetalMiningMod.staticRandomGlimmersteelChance) == 0) {
+                                tempid = 697;
+                            }
+                        }
+                        if(MoonMetalMiningMod.staticRandomSeryllChance > 0){
+                            if (tempid == 0 && Server.rand.nextInt(MoonMetalMiningMod.staticRandomSeryllChance) == 0) {
+                                tempid = 837;
+                            }
+                        }
+                        if (tempid != 0){
+                            if (tilex < 0 && tiley < 0) {
+                                final Item metal = ItemFactory.createItem(tempid, (float)power, (String)null);
+                                metal.setLastOwnerId(performer.getWurmId());
+                                return metal;
+                            }
+                            final Item metal = ItemFactory.createItem(tempid, (float)power, (float)(createtilex * 4 + Server.rand.nextInt(4)), (float)(createtiley * 4 + Server.rand.nextInt(4)), Server.rand.nextFloat() * 360.0f, surfaced, rarity, -10L, (String)null);
+                            metal.setLastOwnerId(performer.getWurmId());
+                            performer.getCommunicator().sendNormalServerMessage("You find a chunk of a mysterious metal.");
                         }
                         return method.invoke(object, args);
                     }
@@ -103,6 +123,7 @@ public class MoonMetalMiningMod implements WurmMod, Configurable, PreInitable {
             MethodInfo mi = caveWallClass.getMethod("action", actionMethodDesc).getMethodInfo();
             CodeAttribute ca = mi.getCodeAttribute();
             ConstPool constPool= ca.getConstPool();
+            int capRef = constPool.addIntegerInfo(newVeinCap);
 
             CodeIterator codeIterator = ca.iterator();
             while(codeIterator.hasNext()) {
@@ -111,14 +132,16 @@ public class MoonMetalMiningMod implements WurmMod, Configurable, PreInitable {
                 int op = codeIterator.byteAt(pos);
                 int siPusharg = codeIterator.u16bitAt(pos+1);
                 
-                //sipush(3) if_icmpeq(3) iload(2) sipush(3) if_icmpne(3) TOOVERWRITE(bipush(2) to iload(2)) TOGET(istore(2)) overwritepos: 14-15 getpos = 17
-                int resourceRef = codeIterator.byteAt(pos+17);
-                
+                //bipush(2) if_icmple(3) iload(2)  <pos>sipush(3) if_icmpeq(3) iload(2) sipush(3) if_icmpne(3) TOOVERWRITE(bipush(2) to iload(2)) TOGET(istore(2)) overwritepos: 14-15 getpos = 17
                 
                 if (op == CodeIterator.SIPUSH && siPusharg == 693){
                     logger.log(Level.INFO, "Found bytecode pattern for moon metal veins");
-                    codeIterator.writeByte(CodeIterator.ILOAD, pos+14);
-                    codeIterator.writeByte(resourceRef, pos+15);
+                    codeIterator.insertGap(pos+14, 1);
+                    codeIterator.writeByte(CodeIterator.LDC_W, pos+14);
+                    codeIterator.write16bit(capRef, pos+15);
+                    codeIterator.insertGap(pos-7, 1);
+                    codeIterator.writeByte(CodeIterator.LDC_W, pos-7);
+                    codeIterator.write16bit(capRef, pos-6);
                     logger.log(Level.INFO, "Moon metal vein cap removed");
                     break;
                 }
@@ -130,7 +153,8 @@ public class MoonMetalMiningMod implements WurmMod, Configurable, PreInitable {
             throw new HookException(e);
         }
         catch(BadBytecode e){
-            e.printStackTrace();
+            System.out.println("BAD BYTECODE ERROR ----- ");
+            //e.printStackTrace();
         }
     }
 }
